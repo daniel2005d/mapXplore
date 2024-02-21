750911f045035cca190a7954f6ca8b45cd4bfa88
Cyb3rb0b <daniel@daniel.com>
2024-02-21 17:23:03 -0500

    Remove unnecesary files

--- searchinformation.py
+++ searchinformation.py
@@ -10,17 +10,4 @@ import sys
 
 
 
-# class Searcher(DbConnector):
-#     def __init__(self, *argv) -> None:
-#         super().__init__(*argv)
-#         self._type = argv[0].type
-#         self._value = argv[0].value
-#         connection = self._createDBEngine()
-#         self._console = SearchConsole()
-#         self._console.set_config(connection=connection)
-
-#     def search(self):
-#         self._console.print_information()
-#         sys.exit(self._console.cmdloop())
-
 console.main()
